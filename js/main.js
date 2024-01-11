ea41dc5f3884c7ed70dee715c2d25b588e4423bf
ArjayBuen <arjaybuen10@gmail.com>
2024-01-11 21:30:33 +0800

    continuation coding the landing page

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// FAQ Accordion
+document.addEventListener("DOMContentLoaded",function(){
+    const faqContainer=document.querySelector(".faq-content");
+    faqContainer.addEventListener('click',(e)=>{
+        const groupHeader=e.target.closest('.faq-group-header');
+        if(!groupHeader) return;
+
+        const group=groupHeader.parentElement;//targets faq-group
+        const groupBody=group.querySelector('.faq-group-body');
+        const icon=groupHeader.querySelector('i');
+        
+        //Toggle icon
+        icon.classList.toggle('fa-plus');
+        icon.classList.toggle('fa-minus');
+
+        //Toggle visibilty of body
+        groupBody.classList.toggle('open');
+
+        //Close other faq bodies
+        const otherGroups=faqContainer.querySelectorAll('.faq-group');
+        otherGroups.forEach((otherGroup)=>{
+            if(otherGroup !== group){
+                const otherGroupBody=otherGroup.querySelector('.faq-group-body');
+                const otherIcon=otherGroup.querySelector('.faq-group-header i');
+
+                otherGroupBody.classList.remove('open');
+                otherIcon.classList.remove('fa-minus');
+                otherIcon.classList.add('fa-plus');
+            }
+        });
+    });
+});
